90a3ec5d20de2df7f745047fbe404f1c79525a4f
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2023-10-14 15:42:16 +0900

    [Silver III] Title: N과 M (2), Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,38 +2,40 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * https://www.acmicpc.net/problem/15650
- * N과 M (2)
- * @author SSAFY
- *
- */
 public class Main {
+	static int N, M, intput[], output[];
 	static StringBuilder sb;
-	static int N, M, output[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());		
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		
+//		intput = new int[N];
+		sb = new StringBuilder();
 		output = new int[M];
 		
-		comb(0, 1);
+		
+				
+		comb(0, 0);
 		System.out.println(sb);
 	}
 	
 	private static void comb(int cnt, int start) {
 		if (cnt == M) {
-			for (int o : output) {
-				sb.append(o).append(" ");
+			for (int i = 0; i < M; i++) {
+//				System.out.print(output[i] + " ");
+				sb.append(output[i]).append(" ");
 			}
+//			System.out.println();
 			sb.append("\n");
 			return;
 		}
 		
-		for (int i = start; i <= N; i++) {
-			output[cnt] = i;
+		for (int i = start; i < N; i++) {
+//			output[cnt] = intput[i];
+			output[cnt] = i + 1;
 			comb(cnt + 1, i + 1);
 		}
 	}
