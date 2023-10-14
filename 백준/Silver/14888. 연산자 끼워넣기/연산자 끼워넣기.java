6b75529380e6e5f947b7afd169cd09fcce642e77
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2023-10-14 17:21:28 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 128 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,63 +1,66 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static int n;
-    public static int[] arr = new int[11];
+public class Main {
+	public static int n;
+	public static int[] numbers = new int[11];
 
-    public static int min = (int) 1e9;
-    public static int max = (int) -1e9;
+	public static int min = (int) 1e9;
+	public static int max = (int) -1e9;
 
-    public static int add, sub, mul, div;
+	public static int add, sub, mul, div;
 
-    public static void calculation(int i, int now) {
-        if (i == n) {
-            min = Math.min(min, now);
-            max = Math.max(max, now);
-        } else {
-            if (add > 0) {
-                add--;
-                calculation(i + 1, now + arr[i]);
-                add++;
-            }
-            if (sub > 0) {
-                sub--;
-                calculation(i + 1, now - arr[i]);
-                sub++;
-            }
-            if (mul > 0) {
-                mul--;
-                calculation(i + 1, now * arr[i]);
-                mul++;
-            }
-            if (div > 0) {
-                div--;
-                calculation(i + 1, now / arr[i]);
-                div++;
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stk = null;
+		n = Integer.parseInt(bf.readLine());
 
-        n = Integer.parseInt(bf.readLine());
+		st = new StringTokenizer(bf.readLine());
+		for (int i = 0; i < n; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
+		}
 
-        stk = new StringTokenizer(bf.readLine(), " ");
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(stk.nextToken());
-        }
+		st = new StringTokenizer(bf.readLine());
+		add = Integer.parseInt(st.nextToken());
+		sub = Integer.parseInt(st.nextToken());
+		mul = Integer.parseInt(st.nextToken());
+		div = Integer.parseInt(st.nextToken());
 
-        stk = new StringTokenizer(bf.readLine(), " ");
-        add = Integer.parseInt(stk.nextToken());
-        sub = Integer.parseInt(stk.nextToken());
-        mul = Integer.parseInt(stk.nextToken());
-        div = Integer.parseInt(stk.nextToken());
+		calculation(1, numbers[0]);
 
-        calculation(1, arr[0]);
+		System.out.println(max);
+		System.out.println(min);
+	}
 
-        System.out.println(max);
-        System.out.println(min);
-    }
+	public static void calculation(int cnt, int result) {
+		if (cnt == n) {
+			min = Math.min(min, result);
+			max = Math.max(max, result);
+			return;
+		}
+
+		if (add > 0) {
+			add--;
+			calculation(cnt + 1, result + numbers[cnt]);
+			add++;
+		}
+		if (sub > 0) {
+			sub--;
+			calculation(cnt + 1, result - numbers[cnt]);
+			sub++;
+		}
+		if (mul > 0) {
+			mul--;
+			calculation(cnt + 1, result * numbers[cnt]);
+			mul++;
+		}
+		if (div > 0) {
+			div--;
+			calculation(cnt + 1, result / numbers[cnt]);
+			div++;
+		}
+	}
 }
