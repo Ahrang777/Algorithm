d47203588529740ce0f852219df6a68a265671b8
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2023-11-07 15:49:34 +0900

    [Gold IV] Title: 거짓말, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N, M;
 	static List<Integer>[] graph; // 같은 파티에 나왔는지 연결 상태
-	static boolean[] visited;	// 방문처리, 방문했으면 진실 전파된것
+	static boolean[] visited;	// bfs 방문처리, 방문했으면 진실 전파된것
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,6 +17,7 @@ public class Main {
 		int result = 0;
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		
 		graph = new ArrayList[N + 1];
 		visited = new boolean[N + 1];
 		for (int i = 1; i <= N; i++) {
@@ -25,24 +26,32 @@ public class Main {
 
 		st = new StringTokenizer(br.readLine());
 
-		int truthNum = Integer.parseInt(st.nextToken());
-		int[] truthArr = new int[truthNum];
+		int truthNum = Integer.parseInt(st.nextToken());	// 진실을 아는 사람의 수 
+		int[] truthArr = new int[truthNum];	// 진실을 아는 사람의 번호
+		
 		for (int i = 0; i < truthNum; i++) {
 			truthArr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		List<Integer>[] party = new ArrayList[M];
+		List<Integer>[] party = new ArrayList[M];	// 각 파티마다 참여한 사람의 번호 저장
 		for (int i = 0; i < M; i++) {
 			party[i] = new ArrayList<>();
 		}
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int participantNum = Integer.parseInt(st.nextToken());
+			
+			// 현재 파티에 참여한 사람의 수
+			int participantCnt = Integer.parseInt(st.nextToken());
+			
+			// 각 파티에는 무조건 1명이상 참여하기에 처음 나온 사람의 번호를 시작점으로 한다
 			int start = Integer.parseInt(st.nextToken());
+			
 			party[i].add(start);
-			for (int j = 1; j < participantNum; j++) {
+			for (int j = 1; j < participantCnt; j++) {
 				int x = Integer.parseInt(st.nextToken());
+				
+				// 처음 나온 사람의 번호를 기준으로 그래프 연결
 				graph[start].add(x);
 				graph[x].add(start);
 				party[i].add(x);
@@ -55,13 +64,16 @@ public class Main {
 		}		
 		
 		for (int i = 0; i < M; i++) {
+			// 해당 파티에서 과장된 이야기를 하였는가
 			boolean isExaggeration = true; 
 			for (int n : party[i]) {
+				// 한번이라도 진실을 아는 사람이 등장하면 진실만 말해야 함
 				if (visited[n]) {
 					isExaggeration = false;
 				}
 			}
 			
+			// 과장된 이야기를 한 파티 수 증가
 			if (isExaggeration) {
 				result++;
 			}
