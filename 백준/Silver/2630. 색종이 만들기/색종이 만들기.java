85461ecc749797e161b4604e355883b59e60cc14
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2023-11-28 00:11:55 +0900

    [Silver II] Title: 색종이 만들기, Time: 172 ms, Memory: 15652 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, whiteCnt, blueCnt, map[][];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        cut(0, 0, N);
+
+        sb.append(whiteCnt).append("\n").append(blueCnt);
+        System.out.println(sb);
+    }
+
+    private static void cut(int x, int y, int len) {
+        int white = 0, blue = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (map[x + i][y + j] == 0) {
+                    white++;
+                } else {
+                    blue++;
+                }
+            }
+        }
+
+        if (white == len * len) {
+            whiteCnt++;
+            return;
+        }
+
+        if (blue == len * len) {
+            blueCnt++;
+            return;
+        }
+
+        cut(x, y, len / 2);
+        cut(x + len / 2, y, len / 2);
+        cut(x, y + len / 2, len / 2);
+        cut(x + len / 2, y + len / 2, len / 2);
+    }
+
+}
