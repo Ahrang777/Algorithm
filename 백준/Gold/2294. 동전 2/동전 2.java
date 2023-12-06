74de5f22af85530342c3d7beabfd6e222e625682
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2023-12-07 00:07:22 +0900

    [Gold V] Title: 동전 2, Time: 132 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -3,7 +3,41 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int N, K, dp[];
+    static int N, K;
+    static int[] coin, dp;
+    static final int INF = 10000001;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        dp = new int[K + 1];
+        Arrays.fill(dp, INF);
+        dp[0] = 0;
+
+        coin = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+            if(coin[i] <= K) {
+                dp[coin[i]] = 1;
+            }
+        }
+
+        for (int i = 1; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i - coin[j] > 0 && dp[i] > dp[i - coin[j]] + 1) {
+                    dp[i] = dp[i - coin[j]] + 1;
+                }
+            }
+        }
+        System.out.println(dp[K] == INF ? -1 : dp[K]);
+    }
+
+    /*static int N, K, dp[];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -55,5 +89,5 @@ public class Main {
                 q.offer(next);
             }
         }
-    }
+    }*/
 }
