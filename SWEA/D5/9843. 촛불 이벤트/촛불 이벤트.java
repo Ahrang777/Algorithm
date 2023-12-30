e082fc1fc59d29a8579b790a75423514103b2c2e
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2023-12-31 02:38:27 +0900

    [D5] Title: 촛불 이벤트, Time: 306 ms, Memory: 48,600 KB -BaekjoonHub

--- SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.java
+++ SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.java
@@ -1,40 +1,41 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Solution
-{
-    public static void main(String args[]) throws Exception
-    {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
+public class Solution {
+    static long N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
+        int T = Integer.parseInt(br.readLine());
 
-            long N = sc.nextLong();
+        for (int tc = 1; tc <= T; tc++) {
+            sb.append("#").append(tc).append(" ");
 
-            long L = 1;             // 정답이 존재하는 가능성이 있는 왼쪽 끝
-            long R = 10000000000L;  // 정답이 존재하는 가능성이 있는 오른쪽 끝
-            long res = 0;           // 내가 찾은 최고의 정답
-            while(L <= R) {
-                long mid = (L + R) / 2;
-                long value = mid * (mid + 1) / 2;
+            N = Long.parseLong(br.readLine());
+            long start = 1; // 정답이 존재하는 가능성이 있는 왼쪽 끝
 
-                if(N >= value) {    // * 가능한 케이스
-                    res = mid;      // 정답을 갱신
-                    L = mid + 1;    // 오른쪽 절반에 대해 추가 탐색
-                } else {            // * 불가능한 케이스
-                    R = mid - 1;    // 왼쪽 절반에 대해 추가 탐색
+            // K (K + 1) / 2 <= N을 생각했을 때 N은 최대 10 ^ 18
+            // K (K + 1) >> K ^ 2 == 2 * N >> 최대 2 * 10 ^ 18
+            // K = 2 * 10 ^ 9 까지 고려하는게 안전
+            long end = 10000000000L;    // 정답이 존재하는 가능성이 있는 오른쪽 끝
+            long result = 0;
+
+            while (start <= end) {
+                long mid = (start + end) / 2;
+
+                if ((mid * (mid + 1) / 2) <= N) {
+                    result = Math.max(result, mid);
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
             }
 
-            long value = res * (res + 1) / 2;
-            if(N != value) {
-                res = -1;
-            }
-            System.out.printf("#%d %d\n", test_case, res);
-
+            sb.append((result * (result + 1) / 2) == N ? result : -1).append("\n");
         }
+
+        System.out.print(sb);
     }
 }
