2fb7030b80be59a61a5a96822724b5842db595c8
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2023-12-31 04:04:17 +0900

    [D4] Title: 사탕 가방, Time: 133 ms, Memory: 23,920 KB -BaekjoonHub

--- SWEA/D4/11446. 사탕 가방/사탕 가방.java
+++ SWEA/D4/11446. 사탕 가방/사탕 가방.java
@@ -28,22 +28,27 @@ public class Solution {
                 candy[i] = Long.parseLong(st.nextToken());
                 max = Math.max(max, candy[i]);
             }
-            
+
             // 이런 start, end의 초기값을 주의하자
+            // 가방의 개수 시작, 종료 범위
             long start = 1;
-            long end = max;
+            long end = max; // 범위를 10 ^ 18으로 해도되는데 최적화를 하는것. 제일 많은 사탕의 개수보다는 가방을 더 많이 만들 수 없다.
             long result = 0;
             while (start <= end) {
                 long mid = (start + end) / 2;
                 long total = 0L;
 
+                // total = 모든 사탕을 가방 개수로 나눈(모든 가방의 구성이 같아야 하므로) 합
+                // total은 각 사탕의 경우 모든 가방에 사탕을 분배한 경우의 가방에 들어갈 사탕 개수의 합
+                // total = 각 가방에 들어갈 수 있는 사탕의 개수이다. 따라서 total >= M이면 조건을 만족해서 분배할 수 있는 가방의 개수이다.
                 for (int i = 0; i < N; i++) {
                     total += (candy[i] / mid);
                 }
 
                 if (total >= M) {
                     start = mid + 1;
-                    result = Math.max(result, mid);
+//                    result = Math.max(result, mid);
+                    result = mid;
                 } else {
                     end = mid - 1;
                 }
