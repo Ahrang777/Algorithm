db520790d469b2d9bfe7dc989a295ff30d418515
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-03-04 20:37:38 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,42 +1,49 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, intput[], output[];
-	static StringBuilder sb;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());		
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-//		intput = new int[N];
-		sb = new StringBuilder();
-		output = new int[M];
-		
-		
-				
-		comb(0, 0);
-		System.out.println(sb);
-	}
-	
-	private static void comb(int cnt, int start) {
-		if (cnt == M) {
-			for (int i = 0; i < M; i++) {
-//				System.out.print(output[i] + " ");
-				sb.append(output[i]).append(" ");
-			}
-//			System.out.println();
-			sb.append("\n");
-			return;
-		}
-		
-		for (int i = start; i < N; i++) {
-//			output[cnt] = intput[i];
-			output[cnt] = i + 1;
-			comb(cnt + 1, i + 1);
-		}
-	}
+    static int N, M;
+    static boolean[] visited;
+    static int[] output;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N];
+        output = new int[M];
+
+        combination(0, 0);
+
+        System.out.println(sb.toString());
+    }
+
+    static void combination(int cnt, int start) {
+        if (cnt == M) {
+            for (int o : output) {
+                sb.append(o).append(" ");
+            }
+            sb.append("\n");
+
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            visited[i] = true;
+            output[cnt] = i + 1;
+            combination(cnt + 1, i + 1);
+            visited[i] = false;
+        }
+    }
 }
