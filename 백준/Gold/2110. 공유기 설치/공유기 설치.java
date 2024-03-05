06766484963154177885db860568e59f695c115c
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-03-05 20:15:38 +0900

    [Gold IV] Title: 공유기 설치, Time: 292 ms, Memory: 29212 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -5,15 +5,14 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, C, max, homes[];
-
+    static int N, C;
+    static int[] homes;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
-        max = Integer.MIN_VALUE;
 
         homes = new int[N];
 
@@ -23,32 +22,30 @@ public class Main {
 
         Arrays.sort(homes);
 
-        // 가장 가까운 공유기 사이 거리
-        int start = 1;
-        int end = homes[N - 1] - homes[0];
+        int result = Integer.MIN_VALUE;
 
+        // 두 공유기 사이 가장 인접한 거리
+        int start = 0, end = homes[N - 1] - homes[0];
         while (start <= end) {
             int mid = (start + end) / 2;
-
-            int cnt = 1;
             int prev = 0;
+            int cnt = 1;
 
             for (int i = 1; i < N; i++) {
                 if (homes[i] - homes[prev] >= mid) {
-                    cnt++;
                     prev = i;
+                    cnt++;
                 }
             }
 
-            if (cnt >= C) {
+            if (cnt >= C) { // 가장 인접한 거리 증가해도 된다.
+                result = Math.max(mid, result);
                 start = mid + 1;
-                max = Math.max(max, mid);
             } else {
                 end = mid - 1;
             }
         }
 
-        System.out.println(max);
+        System.out.println(result);
     }
-
 }
