b6220e027a356adc5eb37d0e53886f844052ceb6
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-03-08 01:07:25 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 2140 ms, Memory: 678420 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -4,14 +4,6 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int index, value;
-
-        public Node(int index, int value) {
-            this.index = index;
-            this.value = value;
-        }
-    }
     static int N, L;
     static int[] arr, D;
     public static void main(String[] args) throws IOException {
@@ -28,20 +20,22 @@ public class Main {
         st = new StringTokenizer(br.readLine());
 
         // index, value
-        // 덱에는 항상 현재까지 범위에 해당하는 최소의 값들로만 유지, 값은 같지만 index가 다른 경우들이 있다. 모두 저장
         Deque<int[]> dq = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
             int n = Integer.parseInt(st.nextToken());
 
-            // 맨뒤에 추가해 나가기에 맨뒤가 최신
-            // 가장 작은 값만 남도록 처리
-            while (!dq.isEmpty() && dq.peekLast()[1] > n) {
-                dq.pollLast();
-            }
-
             // 해당하지 않는 index 값들인 경우 제거 >> 앞이 가장 오래되었기에 앞에서부터 확인
+            // 범위내의 값만 남도록 정리
             while (!dq.isEmpty() && dq.peekFirst()[0] < i - L + 1) {
                 dq.pollFirst();
+            }
+            
+            // 맨뒤에 추가해 나가기에 맨뒤가 최신
+            // 현재 있는 값들은 모두 범위내에 있는 값들이기에 맨 마지막의 값은 현재 넣을 n보다 먼저 범위 밖으로 나간다.
+            // 따라서 n이 맨 마지막(가장 최근) value보다 큰 값이될때까지 dq 뒤에서부터 제거 
+            // 즉, 현재 있는 값들은 범위 내에 있는 값들이라 n보다 큰 값이면 범위내 최솟값이 될 수 없으므로 n보다 큰 것들 제거
+            while (!dq.isEmpty() && dq.peekLast()[1] > n) {
+                dq.pollLast();
             }
 
             dq.offerLast(new int[]{i, n});
