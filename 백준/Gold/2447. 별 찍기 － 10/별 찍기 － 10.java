35f010cf1b2e623e29c6445ade799a45c39a7706
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-03-11 03:05:24 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 336 ms, Memory: 47992 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -1,60 +1,54 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-	static int N;
-	static String[][] map;
+    static int N;
+    static char[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
 
-		map = new String[N][N];
+        printStar(N, 0, 0);
 
-		star(0, 0, N);
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				sb.append(map[i][j] != null ? map[i][j] : " ");
-			}
-			sb.append("\n");
-		}
-		
-		System.out.println(sb);				
-	}
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] != '*') {
+                    sb.append(' ');
+                    continue;
+                }
+                sb.append(map[i][j]);
+            }
+            sb.append("\n");
+        }
 
-	/**
-	 * 
-	 * @param x   시작점 x좌표
-	 * @param y   시작점 y좌표
-	 * @param len 한변 길이
-	 */
-	private static void star(int x, int y, int len) {
-		if (len == 1) {
-			map[x][y] = "*";
-			return;
-		}
+        System.out.println(sb);
+    }
 
-		/*int size = len / 3;
-		int cnt = 0;
-		for (int i = x; i < x + len; i += size) {
-			for (int j = y; j < y + len; j += size) {
-				if (cnt != 4) {
-					star(i, j, size);
-				}		
-				
-				cnt++;
-			}
-		}*/
-		
-		len = len / 3;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (i == 1 && j == 1)	continue;
-				
-				star(x + i * len, y + j * len, len);
-			}
-		}
-	}
+    /**
+     *
+     * @param len 길이
+     * @param x 시작점 x좌표
+     * @param y 시작점 y좌표
+     */
+    private static void printStar(int len, int x, int y) {
+        if (len == 1) {
+            map[x][y] = '*';
+            return;
+        }
+
+        int step = len / 3;
+        for (int i = 0; i < len; i += step) {
+            for (int j = 0; j < len; j += step) {
+                if (i == step && j == step) {
+                    continue;
+                }
+
+                printStar(step, x + i, y + j);
+            }
+        }
+    }
 }
