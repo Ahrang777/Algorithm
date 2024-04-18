4aa3157d82f4a9c20bb0898ee3b7974170b433cb
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-04-19 01:13:27 +0900

    [Gold IV] Title: N-Queen, Time: 5260 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,40 +1,45 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int N, result, output[];
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		output = new int[N];
+    static int N, result;
+    static int[] output;    // 해당 행(idx)의 열(요소)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        output = new int[N];
 
-		dfs(0);
-		System.out.println(result);
-	}
+        solve(0);
+        System.out.println(result);
+    }
 
-	private static void dfs(int cnt) {
-		if (cnt == N) {
-			result++;
-			return;
-		}
+    private static void solve(int row) {
+        if (row == N) {
+            result++;
+            return;
+        }
 
-		for (int i = 0; i < N; i++) {
-			output[cnt] = i;
-			if (!isValid(cnt))	continue;
-			
-			dfs(cnt + 1);
-		}
-	}
+        // col >> i
+        for (int i = 0; i < N; i++) {
+            output[row] = i;
+            if (!isValid(row)) {
+                continue;
+            }
+            solve(row + 1);
+        }
+    }
 
-	// 가로, 세로, 대각선 체크
-	private static boolean isValid(int cnt) {
-		for (int i = 0; i < cnt; i++) {
-			// 세로 체크
-			if (output[i] == output[cnt])	return false;
-			if (Math.abs(i - cnt) == Math.abs(output[i] - output[cnt]))	return false;
-		}
+    // 행의 경우 row를 1씩 증가하면서 처리했기에 output 의 idx를 1씩 증가하면서 처리했기에 ok
+    private static boolean isValid(int row) {
+        for (int i = 0; i < row; i++) {
+            // col 체크
+            if (output[i] == output[row])	return false;
 
-		return true;
-	}	
+            // 대각선 체크 >> row의 차이 절대값, col 차이 절대값 동일하면 대각선 >> ex) row: -2, col: +2
+            if (Math.abs(i - row) == Math.abs(output[i] - output[row]))	return false;
+        }
+
+        return true;
+    }
 }
