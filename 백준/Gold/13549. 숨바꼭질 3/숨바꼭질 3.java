f4d4086367996fc6e9f1c9a9ec69b84a693f1c84
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-05-28 16:19:50 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 284 ms, Memory: 61592 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,45 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
- 
-public class Main {    
- 
-    static int min = Integer.MAX_VALUE;
-    static int n, k;
-    static boolean[] visited;
-    static int max = 100000;
-    
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        
-        n = scan.nextInt();
-        k = scan.nextInt();
-        
-        visited = new boolean[max + 1];
-        bfs();
-        System.out.println(min);
-    }
-    
-    public static void bfs() {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(n, 0));
-        
-        while(!q.isEmpty()) {
-            Node node = q.poll();
-            visited[node.x] = true;
-            if(node.x == k) min = Math.min(min, node.time);
-            
-            if(node.x * 2 <= max && visited[node.x * 2] == false) q.offer(new Node(node.x * 2, node.time));
-            if(node.x + 1 <= max && visited[node.x + 1] == false) q.offer(new Node(node.x + 1, node.time + 1));
-            if(node.x - 1 >= 0 && visited[node.x - 1] == false) q.offer(new Node(node.x - 1, node.time + 1));
+
+public class Main {
+    static class Node {
+        int index, time;
+
+        public Node(int index, int time) {
+            this.index = index;
+            this.time = time;
         }
     }
-    
-    public static class Node {
-        int x;
-        int time;
-        
-        public Node(int x, int time) {
-            this.x = x;
-            this.time = time;
+    static int N, K;
+    static int max = 100000;
+    static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        bfs(N, K);
+        System.out.println(min);
+    }
+
+    private static void bfs(int now, int target) {
+        Queue<Node> q = new ArrayDeque<>();
+        boolean[] visited = new boolean[max + 1];
+
+        q.offer(new Node(now, 0));
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            int current = node.index;
+            int time = node.time;
+            visited[current] = true;
+
+            if (current == target) {
+                min = Math.min(min, time);
+            }
+
+            if (current - 1 >= 0 && !visited[current - 1]) {
+                q.offer(new Node(current - 1, time + 1));
+            }
+            if (current + 1 <= max && !visited[current + 1]) {
+                q.offer(new Node(current + 1, time + 1));
+            }
+            if (2 * current <= max && !visited[2 * current]) {
+                q.offer(new Node(2 * current, time));
+            }
         }
     }
 }
