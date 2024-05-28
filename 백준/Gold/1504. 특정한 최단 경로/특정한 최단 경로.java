8657b3cc18f20b69842f5194d4fd3bd002736983
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-05-28 13:35:58 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 504 ms, Memory: 57388 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,110 +1,78 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Node implements Comparable<Node> {
-        int to, d;
+	private static int N, E;
+	public static void main(String... args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stk = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(stk.nextToken());
+		E = Integer.parseInt(stk.nextToken());
+		int[][] arr = new int[N][N];
+		for(int i = 0 ;i < E ; i++) {
+			stk = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(stk.nextToken());
+			int b = Integer.parseInt(stk.nextToken());
+			int c = Integer.parseInt(stk.nextToken());
+			arr[a-1][b-1] = c;
+			arr[b-1][a-1] = c;
+		}
+		stk = new StringTokenizer(br.readLine());
+		int v1 = Integer.parseInt(stk.nextToken())-1;
+		
+		int v2 = Integer.parseInt(stk.nextToken())-1;
+		
+		
+		int result = Math.min(
+		dijkstra(arr, 0, v1) + dijkstra(arr, v1, v2) + dijkstra(arr, v2, N-1),
+		dijkstra(arr, 0, v2) + dijkstra(arr, v2, v1) + dijkstra(arr, v1, N-1)
+		);
+		if(result >= 10000000) {
+			result = -1;
+		}
+		System.out.println(result);
+	}
 
-        public Node(int to, int d) {
-            this.to = to;
-            this.d = d;
-        }
+	private static int dijkstra(int[][] arr, int start, int end) {
+		
+		int[] distance = new int[N];
+		boolean[] visited = new boolean[N];
+		
+		Arrays.fill(distance, 10000000);
+		distance[start] = 0; 
+		
+		for(int i = 0; i < N ; i++) {
+			
+			int min = Integer.MAX_VALUE;
+			int current = 0; 
+			
+			for(int j = 0 ; j < N; j++) {
+				
+				if(!visited[j] && min>distance[j]) {
+					
+					min = distance[j];
+					current = j;
+					
+				}
+				
+			}
+			
+			visited[current] = true; 
+			
+			if(current == end) break;
+			
+			
+			for(int j = 0; j < N ; j++){
 
-        @Override
-        public int compareTo(Node o) {
-            return Integer.compare(this.d, o.d);
-        }
-    }
-    static int N, E, min;
-    static final int INF = (int) 1e7;
-    static List<Node>[] graph;
-    static boolean[] visited;
-    static int[] d1, dN, dMid;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-
-        graph = new ArrayList[N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            graph[from].add(new Node(to, d));
-            graph[to].add(new Node(from, d));
-        }
-
-        st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
-
-        d1 = new int[N + 1];
-        dMid = new int[N + 1];
-        dN = new int[N + 1];
-
-        Arrays.fill(d1, INF);
-        Arrays.fill(dMid, INF);
-        Arrays.fill(dN, INF);
-
-        dijkstra(1, d1);
-        dijkstra(v1, dMid);
-        dijkstra(N, dN);
-
-        int v1Dist = d1[v1] + dMid[v2] + dN[v2];
-        int v2Dist = d1[v2] + dMid[v2] + dN[v1];
-
-        if (v1Dist >= INF && v2Dist >= INF) {
-            System.out.println(-1);
-        } else {
-            System.out.println(Math.min(v1Dist, v2Dist));
-        }
-    }
-
-    private static void dijkstra(int start, int[] d) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start, 0));
-        d[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            int now = node.to;
-            int dist = node.d;
-
-            if (d[now] < dist)  continue;
-
-            for (Node next : graph[now]) {
-                int cost = d[now] + next.d;
-
-                if (cost < d[next.to]) {
-                    d[next.to] = cost;
-                    pq.offer(new Node(next.to, cost));
-                }
-            }
-        }
-    }
-
-    private static void bfs(int start, int end, int total) {
-        if (start == end) {
-            min = Math.min(min, total);
-            return;
-        }
-
-        for (Node next : graph[start]) {
-            if (visited[next.to])   continue;
-
-            visited[next.to] = true;
-            bfs(next.to, end, total + next.d);
-            visited[next.to] = false;
-        }
-    }
+				if(!visited[j] && arr[current][j] != 0 && distance[j] > min + arr[current][j] ) {
+					distance[j] = min + arr[current][j];
+				}
+			}
+			
+		}
+		return distance[end];
+		
+	}
 }
