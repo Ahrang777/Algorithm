0d5d072f3fae81e34e0c30717f2ff26022e5e2b4
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-07-07 03:42:59 +0900

    [Gold V] Title: 개똥벌레, Time: 260 ms, Memory: 29324 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.java
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -2,46 +2,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, H, imos[];
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
-        N = Integer.parseInt(st.nextToken());
-        H = Integer.parseInt(st.nextToken());
-
-        imos = new int[H];
-
-        for (int i = 0; i < N; i++) {
-            int h = Integer.parseInt(br.readLine());
-            if (i % 2 == 0) {   //  석순 >> 위에서 아래로
-                imos[0]++;
-                imos[h]--;
-            } else {    // 종유석 >> 아래에서 위로
-                imos[H - h]++;
-            }
-        }
-
-        int[] high = new int[H];
-        high[0] = imos[0];
-        int min = high[0];
-
-        for (int i = 1; i < H; i++) {
-            high[i] = high[i - 1] + imos[i];
-            if (min > high[i]) {
-                min = high[i];
-            }
-        }
-
-        int cnt = 0;
-        for (int i = 0; i < H; i++) {
-            if (min == high[i]) {
-                cnt++;
-            }
-        }
-
-        sb.append(min).append(" ").append(cnt);
-        System.out.println(sb);
+    public static void main(String[] args)throws Exception{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer reader=new StringTokenizer(br.readLine());
+    	int n=Integer.parseInt(reader.nextToken());
+    	int h=Integer.parseInt(reader.nextToken());
+    	int[] down=new int[h+1];
+    	for(int i=0;i<n;i++){
+            int j=Integer.parseInt(br.readLine());
+    		if(i%2==0){
+    			down[h-j]--;
+    		}else{
+    			down[j]++;
+    		}
+    	}
+    	int min=down[h]+(n/2);
+    	int count=1;
+    	for(int i=1,k=min;i<h;i++){
+            k+=down[h-i];
+    		if(min>k){
+    			min=k;
+    			count=1;
+    		}else if(min==k){
+    			count+=1;
+    		}
+    	}
+    	System.out.print(min+" "+count);
     }
 }
