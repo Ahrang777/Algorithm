6ff050e2a7549cfba982cd9d17bcec72f6dd7298
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-07-24 15:44:22 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int N;
+    static final long MOD = 1000000000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        // dp[i][j] : 길이가 i 이고 j로 시작하는 계단수의 경우의 수
+        long[][] dp = new long[N + 1][10];
+
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 0; j <= 9; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1] % MOD;
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8] % MOD;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp[N]).sum() % MOD);
+    }
+}
