c6c0e085be3dbf9f8267ae607bc4e495280603f7
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-07-25 11:53:20 +0900

    [Silver III] Title: 이친수, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        // 0: 짝, 1: 홀
+        // dp[i][j] : i자리 이천수들의 짝수, 홀수 개수
+        long[][] dp = new long[N + 1][2];
+
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        if (N >= 2) {
+            dp[2][0] = 1;
+            dp[2][1] = 0;
+
+            // 0으로 끝나는 이진수는 짝수, 1로 끝나는 이진수는 홀수
+            // 0으로 끝나면 다음은 0, 1이 붙을 수 있다. 
+            // 1로 끝나면 다음은 0만 뭍을 수 있다. 
+            // 짝수 -> 짝수, 홀수 / 홀수 -> 짝수
+            // 즉 i자리수의 이친수 중 짝수의 개수는 i -1의 홀수 + 짝수 개수
+            // i자리수의 이친수 중 홀수의 개수는 i - 1의 짝수 개수
+            for (int i = 3; i <= N; i++) {
+                dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+                dp[i][1] = dp[i - 1][0];
+            }
+        }
+
+        System.out.println(dp[N][0] + dp[N][1]);
+    }
+}
