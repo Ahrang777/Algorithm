8b15148d8b5cc79797232ac05c787a58086a4bde
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-07-27 14:44:19 +0900

    [Silver I] Title: 카드 구매하기, Time: 152 ms, Memory: 14528 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -17,12 +17,11 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = arr[i];
         }
 
-        dp[1] = arr[1];
-
         for (int i = 2; i <= N; i++) {
-            for (int j = 0; j < i; j++) {
+            for (int j = 1; j < i; j++) {
                 dp[i] = Math.max(dp[j] + arr[i - j], dp[i]);
             }
         }
