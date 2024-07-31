a3edc34e7e406bf3bbd1472e590fa5beba45e5f0
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-07-31 18:12:15 +0900

    [Gold IV] Title: 미로만들기, Time: 132 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.java
+++ 백준/Gold/2665. 미로만들기/미로만들기.java
@@ -2,86 +2,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n;
+    static char[][] map;
+
+    static int[] dr = { -1, 1, 0, 0 };
+    static int[] dc = { 0, 0, -1, 1 };
+
     static class Node {
-        int x, y;
+        int r, c;
 
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
+        Node(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
     }
-    static int N;
-    static int[][] map;
-    static int[][] visited;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static final int B = 0;
-    static final int W = 1;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        map = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < N; j++) {
-                map[i][j] = str.charAt(j) - '0';
-            }
-        }
-
-        bfs(0, 0);
-
-        System.out.println(visited[N - 1][N - 1] - 1);
+    static boolean outRange(int r, int c) {
+        return r < 0 || r >= n || c < 0 || c >= n;
     }
 
-    private static void bfs(int x, int y) {
-        Queue<Node> q = new ArrayDeque<>();
-        visited = new int[N][N];
+    static int getChangeCnt() {
+        Deque<Node> q = new ArrayDeque<>();
+        q.offer(new Node(0, 0));
 
-        q.offer(new Node(x, y));
-        visited[x][y] = map[x][y] == B ? 2 : 1;
+        int[][] changeCount = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(changeCount[i], -1);
+        }
+        changeCount[0][0] = 0;
 
         while (!q.isEmpty()) {
-            Node now = q.poll();
-            int nowX = now.x;
-            int nowY = now.y;
+            Node curr = q.poll();
 
-            for (int i = 0; i < 4; i++) {
-                int nx = nowX + dx[i];
-                int ny = nowY + dy[i];
+            if (curr.r == n - 1 && curr.c == n - 1) {
+                return changeCount[n - 1][n - 1];
+            }
 
-                if (!isRange(nx, ny)) {
-                    continue;
+            for (int dir = 0; dir < 4; dir++) {
+                int nextR = curr.r + dr[dir];
+                int nextC = curr.c + dc[dir];
+                if (outRange(nextR, nextC)) continue;
+                if (changeCount[nextR][nextC] != -1) continue;
+
+                if (map[nextR][nextC] == '1') {
+                    q.addFirst(new Node(nextR, nextC));
+                    changeCount[nextR][nextC] = changeCount[curr.r][curr.c];
+                } else {
+                    q.addLast(new Node(nextR, nextC));
+                    changeCount[nextR][nextC] = changeCount[curr.r][curr.c] + 1;
                 }
-
-                if (map[nx][ny] == W) { // 흰색
-                    if (visited[nx][ny] == 0) { // 첫 방문
-                        visited[nx][ny] = visited[nowX][nowY];
-                        q.offer(new Node(nx, ny));
-                    } else {    // 앞서 반복한적 있는 경우
-                        if (visited[nx][ny] > visited[nowX][nowY]) {
-                            visited[nx][ny] = visited[nowX][nowY];
-                            q.offer(new Node(nx, ny));
-                        }
-                    }
-                } else {    // 검정색
-                    if (visited[nx][ny] == 0) { // 첫 방문
-                        visited[nx][ny] = visited[nowX][nowY] + 1;
-                        q.offer(new Node(nx, ny));
-                    } else {    // 앞서 반복한적 있는 경우
-                        if (visited[nx][ny] > visited[nowX][nowY] + 1) {
-                            visited[nx][ny] = visited[nowX][nowY] + 1;
-                            q.offer(new Node(nx, ny));
-                        }
-                    }
-                }
-
             }
         }
+
+        return -1;
     }
 
-    private static boolean isRange(int x, int y) {
-        return x >= 0 && x < N && y >= 0 && y < N;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        map = new char[n][];
+        for (int i = 0; i < n; i++) {
+            map[i] = br.readLine().toCharArray();
+        }
+
+        System.out.println(getChangeCnt());
     }
 }
