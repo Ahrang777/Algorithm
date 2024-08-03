9b8c7a0bfd3e5b28f91dda3e83149da2fa5dc42b
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-08-03 16:30:57 +0900

    [Gold III] Title: 치즈, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.java
+++ 백준/Gold/2638. 치즈/치즈.java
@@ -1,39 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static class Node {
+    static class Position {
         int x, y;
 
-        public Node(int x, int y) {
+        public Position(int x, int y) {
             this.x = x;
             this.y = y;
         }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Node node = (Node) o;
-            return x == node.x && y == node.y;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
-        }
     }
-    static int N, M, cheeseCnt, map[][];
-    static List<Node> cheese;
+    static int N, M, cnt;
+    static int[][] map;
+    static List<Position> cheeseList;
     static boolean[][] visited;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -41,7 +25,7 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
-        cheese = new ArrayList<>();
+        cheeseList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -49,54 +33,25 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
 
                 if (map[i][j] == 1) {
-                    cheese.add(new Node(i, j));
-                    cheeseCnt++;
+                    cnt++;
+                    cheeseList.add(new Position(i, j));
                 }
             }
         }
 
         int time = 0;
 
-        while (cheeseCnt > 0) {
+        while (cnt != 0) {
             time++;
             visited = new boolean[N][M];
-            // 외부공기 처리 >> 가장자리는 치즈X
-            findOutsideAir(0, 0);
-
-            // 치즈 녹이기
-            melting();
+            init(0, 0);
+            melt();
         }
 
         System.out.println(time);
     }
 
-    private static void melting() {
-        for (int i = 0; i < cheese.size(); i++) {
-            int x = cheese.get(i).x;
-            int y = cheese.get(i).y;
-            int cnt = 0;
-
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-
-                // 주변에 외부 공기 2번 이상 접촉?
-                if (map[nx][ny] == 2) {
-                    cnt++;
-                }
-            }
-
-            // 외부 공기 2번 이상 접촉한 경우 제거
-            if (cnt >= 2) {
-                map[x][y] = 0;
-                cheeseCnt--;
-                cheese.remove(i);
-                i--;    // 현재 치즈가 제거되어 cheese의 크기가 줄어들었기에
-            }
-        }
-    }
-
-    private static void findOutsideAir(int x, int y) {
+    private static void init(int x, int y) {
         visited[x][y] = true;
         map[x][y] = 2;
 
@@ -104,12 +59,40 @@ public class Main {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
+            // 기존에 이미 2로 처리된 경우가 있을 수 있으므로 visited없이 map[nx][ny] >= 1 로 해결 안됨
             if (!isRange(nx, ny) || visited[nx][ny] || map[nx][ny] == 1) {
                 continue;
             }
 
-            findOutsideAir(nx, ny);
+            init(nx, ny);
         }
+    }
+
+    private static void melt() {
+        for (int i = 0; i < cheeseList.size(); i++) {
+            Position now = cheeseList.get(i);
+
+            if (isMelted(now.x, now.y)) {
+                cnt--;
+                map[now.x][now.y] = 0;
+                cheeseList.remove(i);
+                i--;
+            }
+        }
+    }
+
+    private static boolean isMelted(int x, int y) {
+        int cnt = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (map[nx][ny] == 2) {
+                cnt++;
+            }
+        }
+
+        return cnt >= 2;
     }
 
     private static boolean isRange(int x, int y) {
