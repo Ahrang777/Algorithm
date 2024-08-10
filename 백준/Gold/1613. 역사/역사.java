e92a9aad2f89d0abb01e4b1e850b70b21a265100
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-08-10 15:02:17 +0900

    [Gold III] Title: 역사, Time: 472 ms, Memory: 37500 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.java
+++ 백준/Gold/1613. 역사/역사.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     static int N, K, S;
-    static int[][] graph;
+    static boolean[][] graph;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -12,21 +12,22 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        graph = new int[N + 1][N + 1];
+        graph = new boolean[N + 1][N + 1];
 
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            graph[a][b] = 1;
+            graph[a][b] = true;
         }
 
         for (int i = 1; i <= N; i++) {
             for (int a = 1; a <= N; a++) {
+                if (!graph[a][i]) continue;
                 for (int b = 1; b <= N; b++) {
-                    if (graph[a][i] == 1 && graph[i][b] == 1) {
-                        graph[a][b] = 1;
+                    if (graph[i][b]) {
+                        graph[a][b] = true;
                     }
                 }
             }
@@ -39,12 +40,12 @@ public class Main {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if (graph[a][b] == 0 && graph[b][a] == 0) {
-                sb.append(0).append("\n");
-            } else if (graph[a][b] == 1) {
+            if (graph[a][b]) {
                 sb.append(-1).append("\n");
-            } else if (graph[b][a] == 1) {
+            } else if (graph[b][a]) {
                 sb.append(1).append("\n");
+            } else {
+                sb.append(0).append("\n");
             }
         }
 
