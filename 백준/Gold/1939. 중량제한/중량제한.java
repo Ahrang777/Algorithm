b85fb840ec0f1fa09e511cb477f809fc6a202964
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-08-13 17:20:46 +0900

    [Gold III] Title: 중량제한, Time: 560 ms, Memory: 55444 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -2,91 +2,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Node {
-        int index, dist;
+    static class Node implements Comparable<Node> {
+		int index, dist;
 
-        public Node(int index, int dist) {
-            this.index = index;
-            this.dist = dist;
-        }
-    }
-    static int N, M, F1, F2, s, e;
-    static List<Node>[] graph;
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		public Node(int index, int dist) {
+			this.index = index;
+			this.dist = dist;
+		}
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(o.dist, this.dist);
+		}
+	}
 
-        graph = new List[N + 1];
-        visited = new boolean[N + 1];
-        for (int i = 1; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
+	static int N, M, A, B;
+	static int[] d;
+	static List<Node>[] graph;
+	static final int INF = (int)1e9;
 
-        s = 0;
-        e = 0;
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int dist = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 
-            graph[A].add(new Node(B, dist));
-            graph[B].add(new Node(A, dist));
-            e = Math.max(e, dist);
-        }
+		d = new int[N + 1];
+		Arrays.fill(d, -1);	
 
-        st = new StringTokenizer(br.readLine());
-        F1 = Integer.parseInt(st.nextToken());
-        F2 = Integer.parseInt(st.nextToken());
+		graph = new List[N + 1];
+		for (int i = 0; i <= N; i++) {
+			graph[i] = new ArrayList<>();
+		}
 
-        int answer = 0;
-        while (s <= e) {
-            int mid = (s + e) / 2;
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int dist = Integer.parseInt(st.nextToken());
+			graph[from].add(new Node(to, dist));
+			graph[to].add(new Node(from, dist));
+		}
 
-            // 이동 가능
-            if (isValid(mid)) {
-                answer = mid;
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
+		st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
 
-        System.out.println(answer);
-    }
+		dijkstra();
+		System.out.println(d[B]);
+	}
 
-    private static boolean isValid(int dist) {
-        Queue<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[N + 1];
+	private static void dijkstra() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.offer(new Node(A, INF));
+		d[A] = INF;
 
-        q.offer(F1);
-        visited[F1] = true;
+		while (!pq.isEmpty()) {
+			Node node = pq.poll();
+			int now = node.index;
+			int dist = node.dist;
 
-        while (!q.isEmpty()) {
-            Integer now = q.poll();
+			if (d[now] > dist)	continue;
 
-            if (now == F2) {
-                return true;
-            }
+			for (Node next : graph[now]) {
+				int cost = Math.min(dist, next.dist);
 
-            for (Node next : graph[now]) {
-                int index = next.index;
-                int limit = next.dist;
-
-                if (visited[index] || dist > limit) {
-                    continue;
-                }
-
-                visited[index] = true;
-                q.offer(index);
-            }
-        }
-
-        return false;
-    }
+				if (cost > d[next.index]) {
+					d[next.index] = cost;
+					pq.offer(new Node(next.index, cost));
+				}
+			}
+		}
+	}
 }
