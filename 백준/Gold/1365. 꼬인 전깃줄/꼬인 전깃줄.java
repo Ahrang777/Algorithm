73c929bc676bdc41a48942b0636640dc347dad4d
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-08-14 16:24:41 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 252 ms, Memory: 25844 KB -BaekjoonHub

--- 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.java
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.java
@@ -2,45 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int[] arr, dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
 
-	static int[] dp;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		
-		int[] arr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i=0; i<n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		dp = new int[n+1];
-		int len = 0;
-		int idx = 0;
-		for(int i=0; i<n; i++) {
-			if(arr[i] > dp[len]) {
-				len += 1;
-				dp[len] = arr[i];
-				continue;
-			} 
-			
-			idx = binarySearch(0, len, arr[i]);
-			dp[idx] = arr[i];
-		}
-		
-		System.out.println(n-len);
-				
-				
-	}
-	static int binarySearch(int left, int right, int key) {
-		while(left<right) {
-			int mid = (left+right)/2;
-			if(dp[mid] > key) {
-				right = mid;
-			}else {
-				left = mid+1;
-			}
-		}
-		return right;
-	}
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+        dp = new int[N + 1];    // i 길이를 갖는 증가하는 부분수열의 최대값
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int len = 0;
+        int index = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > dp[len]) {
+                len++;
+                dp[len] = arr[i];
+                continue;
+            }
+
+            index = binarySearch(0, len, arr[i]);
+            dp[index] = arr[i];
+        }
+
+        System.out.println(N - len);
+    }
+
+    private static int binarySearch(int left, int right, int key) {
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (dp[mid] > key) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
 }
