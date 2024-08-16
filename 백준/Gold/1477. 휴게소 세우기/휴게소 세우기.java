83c1a0b4d87006553850c07fd7a0be4f135b811a
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-08-16 16:32:00 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 100 ms, Memory: 14524 KB -BaekjoonHub

--- 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
+++ 백준/Gold/1477. 휴게소 세우기/휴게소 세우기.java
@@ -2,51 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int L = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N + 2];
-        arr[0] = 0;
-        arr[1] = L;
-        st = new StringTokenizer(br.readLine());
-        for (int i = 2; i < N + 2; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(arr);
-
-        System.out.println(lowerBound(1, L, arr, M));
-    }
-
-    static int lowerBound(int left, int right, int[] arr, int M) {
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            int count = getCount(mid, arr);
-            // count 더 늘려야 -> 거리 mid 줄여 -> M과 더 근접
-            // count 같아도  -> 거리 mid 줄여 (더 적은 mid 찾아야)
-            if (count <= M) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-
-    static int getCount(int val, int[] arr) {
-        int count = 0;
-        for (int i = 1; i < arr.length; i++) {
-            int diff = arr[i] - arr[i - 1];
-            count += diff / val;
-            if (diff % val == 0) count--;
-        }
-        return count;
-    }
-    
-    /*static int N, M, L;
+    static int N, M, L;
     static int[] arr, subArr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -75,32 +31,41 @@ public class Main {
         int answer = L - 1;
 
         while (s <= e) {
-            // 현재 구간에서 휴게소가 없는 구간의 값
+            // 현재 구간에서 휴게소가 없는 구간의 최댓값
             int mid = (s + e) / 2;
 
-            // 휴게소 없는 구간의 값이 mid일 경우 추가 설치 가능한 휴게소의 수
+            // 휴게소 없는 구간의 최댓값이 mid일 경우 추가 설치 가능한 휴게소의 수
             int cnt = 0;
 
             for (int i = 1; i < arr.length; i++) {
+                // arr[i] - arr[i - 1] - 1로 마지막에 -1을 해줘야 한다.
+                // 길이 6, mid 2라고 가정하면 6/2 = 3이지만 실제로 설치 가능한 휴게소는 2개이다.
+                // 길이 7, mid 2인 경우 7/2 = 3이고 설치 가능한 휴게소는 3개이다.
+                // 이처럼 -1을 해줘서 mid로 딱 나눠떨어지는 경우를 처리해준다.
+                // 아니면 아래처럼 나누어떨어질때 처리를 해줘도 된다.
+                // cnt += (arr[i] - arr[i - 1]) / mid; 
+                // if ((arr[i] - arr[i - 1]) % mid) == 0) cnt--; 
                 cnt += (arr[i] - arr[i - 1] - 1) / mid;
             }
-
-            if (cnt > M) {
+     
+            if (cnt > M) { // 휴게소 없는 구간의 최댓값이 mid가 되기위해서는 M개보다 많은 휴게소가 필요하면 답이 될 수 없다.
                 s = mid + 1;
             } else {
-                // M개 휴게소 세울수 있는지 여부 아래 경우가 여기에 해당한다.
-                // F, F, F, F, T
-
+                // M개 이하로 세우는 경우여야 가능하다.
+                // M개로 딱 되는거 ok, M개보다 적게 되도 일부를 mid보다 더 적게 나누면 M개 맞출수 있어서 ok
+                
                 // 현재 M값과 동일하게 휴게소를 세우거나 휴게소가 더 적게 세워지는 건 현재 mid가 휴게소 없는 구간의 최댓값이라는 의미
                 // cnt < M인 경우도 괜찮은 이유는
                 // 현재는 최댓값인 mid와 동일하게 나눈다고 가정하여 M보다 작은거지
                 // 하나만 mid로 나누고 나머지는 더 작은 값으로 나우어 휴게소를 M개 세울 수 있다.
-                // 구간 길이 : 1, 1, 1, mid 이런식으로 (mid > 1)
+                // 구간 길이 : 1, 1, 1, mid 이런식으로 (mid > 1) 
                 e = mid - 1;
                 answer = mid;
             }
         }
 
         System.out.println(answer);
-    }*/
+    }
+
+
 }
