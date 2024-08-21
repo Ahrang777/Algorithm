06003a138dc133f4b260ff6c3d63bc0e00bbb7f2
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-08-21 13:32:28 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 524 ms, Memory: 51280 KB -BaekjoonHub

--- 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     static int R, C;
-    static char[][] arr;
+    static char[][] table;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,43 +11,51 @@ public class Main {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        arr = new char[R][C];
+        table = new char[R][C];
 
         for (int i = 0; i < R; i++) {
             String str = br.readLine();
             for (int j = 0; j < C; j++) {
-                arr[i][j] = str.charAt(j);
+                table[i][j] = str.charAt(j);
             }
-        }
-
-        Set<String> set = new HashSet<>();
-        Set<String> tmp = new HashSet<>();
-        for (int j = 0; j < C; j++) {
-            String str = "";
-            for (int i = 0; i < R; i++) {
-                str += arr[i][j];
-            }
-
-            set.add(str);
         }
 
         int answer = 0;
-        for (int i = 0; i < R - 1; i++) {
-            for (String s : set) {
-                tmp.add(s.substring(1));
+
+        int s = 0, e = R - 1;
+
+        while (s <= e) {
+            // 지워지는 행의 번호
+            int mid = (s + e) / 2;
+
+            if (isValid(mid)) {
+                answer = mid + 1;
+                s = mid + 1;
+            } else {
+                e = mid - 1;
             }
-
-            set.clear();
-            set.addAll(tmp);
-            tmp.clear();
-
-            if (set.size() != C) {
-                break;
-            }
-
-            answer++;
         }
 
         System.out.println(answer);
+    }
+
+    private static boolean isValid(int index) {
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < C; i++) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int j = index + 1; j < R; j++) {
+                sb.append(table[j][i]);
+            }
+
+            if (set.contains(sb.toString())) {
+                return false;
+            }
+
+            set.add(sb.toString());
+        }
+
+        return true;
     }
 }
