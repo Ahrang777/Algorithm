ad385bc999f4cde77983de55ad5fd842efac0add
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-09-09 12:54:09 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 64.48 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,70 +1,65 @@
 import java.util.*;
-
 class Solution {
-    static int N;
-    static int serviceResult, totalResult;
-    static int[] output;
-    static boolean[] visited;
-    
+    static int N, M;
+    static List<int[]> discountRates;
     public int[] solution(int[][] users, int[] emoticons) {
-        int[] answer = new int[2];
-        N = emoticons.length;
+        N = users.length;
+        M = emoticons.length;
         
-        output = new int[N];
-        visited = new boolean[N];
+        discountRates = new ArrayList<>();
         
-        permutation(0, users, emoticons);
+        dfs(0, new int[M]);
         
-        answer[0] = serviceResult;
-        answer[1] = totalResult;
-        return answer;
-    }
-    
-    // 중복 순열
-    private void permutation(int cnt, int[][] users, int[] emoticons) {
-        if (cnt == N) {
-            int total = 0;  // 이모티콘 판매액
-            int service = 0;    // 이모티콘 플러스 서비스 가입자 수
+        int maxMembers = 0;
+        int maxTotal = 0;
+        
+        // 할인율 후보마다 계산
+        for (int[] discount : discountRates) {
+            int members = 0;
+            int total = 0;
             
-            // 얻어낸 output을 이용하여 사람마다 판매, 구매 구하기
-            for (int[] user : users) {
-                int percent = user[0];
-                int price = user[1];
-                int totalPrice = 0;
-                
-                for (int i = 0; i < N; i++) {
-                    int out = output[i];
-                    int emoticon = emoticons[i];
-                    
-                    // 이모티콘 구매
-                    if (out >= percent) {
-                        totalPrice += (emoticon / 100) * (100 - out);
+            // 각 사용자 기준마다 계산
+            for (int[] u : users) {
+                // 현재 사용자의 구매한 이모티콘 금액
+                int sum = 0;
+                for (int i = 0; i < M; i++) {
+                    int d = discount[i];
+                    if (d < u[0]) {
+                        continue;
                     }
                     
-                    // 이모티콘 플러스 서비스 가입
-                    if (totalPrice >= price) {
-                        totalPrice = 0;
-                        service++;
+                    sum += (emoticons[i] * (100 - d) / 100);
+                    if (sum >= u[1]) {
+                        sum = 0;
+                        members++;
                         break;
                     }
                 }
                 
-                total += totalPrice;
+                total += sum;
+                
             }
             
-            if (service > serviceResult) {
-                serviceResult = service;
-                totalResult = total;
-            } else if (service == serviceResult && total > totalResult) {
-                totalResult = total;
+            if (maxMembers < members) {
+                maxMembers = members;
+                maxTotal = total;
+            } else if (maxMembers == members && maxTotal < total) {
+                maxTotal = total;
             }
+        }
             
+        return new int[]{maxMembers, maxTotal};
+    }
+    
+    private void dfs(int cnt, int[] discountRate) {
+        if (cnt == M) {
+            discountRates.add(discountRate.clone());
             return;
         }
         
         for (int i = 10; i <= 40; i+=10) {
-            output[cnt] = i;
-            permutation(cnt + 1, users, emoticons);
+            discountRate[cnt] = i;
+            dfs(cnt + 1, discountRate);
         }
     }
 }
