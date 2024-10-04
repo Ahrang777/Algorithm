0822344794d898d727344fdf101e07168d89287b
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-10-04 18:52:11 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,20 +1,43 @@
-SELECT history_id,
-       round(daily_fee * (datediff(end_date, start_date) + 1) * (100 - ifnull(discount_rate, 0)) /
-             100, 0) fee
-FROM (SELECT *,
-             CASE
-                 WHEN datediff(end_date, start_date) + 1 < 7 THEN NULL
-                 WHEN datediff(end_date, start_date) + 1 < 30 THEN '7일 이상'
-                 WHEN datediff(end_date, start_date) + 1 < 90 THEN '30일 이상'
-                 ELSE '90일 이상'
-                 END duration_type
-      FROM car_rental_company_rental_history) a
-         JOIN car_rental_company_car b
-              ON a.car_id = b.car_id
-         LEFT JOIN car_rental_company_discount_plan c
-                   ON c.car_type = b.car_type AND a.duration_type = c.duration_type
-WHERE b.car_type = '트럭'
+# SELECT history_id,
+#        round(daily_fee * (datediff(end_date, start_date) + 1) * (100 - ifnull(discount_rate, 0)) /
+#              100, 0) fee
+# FROM (SELECT *,
+#              CASE
+#                  WHEN datediff(end_date, start_date) + 1 < 7 THEN NULL
+#                  WHEN datediff(end_date, start_date) + 1 < 30 THEN '7일 이상'
+#                  WHEN datediff(end_date, start_date) + 1 < 90 THEN '30일 이상'
+#                  ELSE '90일 이상'
+#                  END duration_type
+#       FROM car_rental_company_rental_history) a
+#          JOIN car_rental_company_car b
+#               ON a.car_id = b.car_id
+#          LEFT JOIN car_rental_company_discount_plan c
+#                    ON c.car_type = b.car_type AND a.duration_type = c.duration_type
+# WHERE b.car_type = '트럭'
+# ORDER BY 2 DESC, 1 DESC
+
+WITH CTE AS (
+    SELECT DISCOUNT_RATE, DURATION_TYPE	
+    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    WHERE CAR_TYPE = '트럭'
+)
+SELECT H.HISTORY_ID, 
+CONVERT((C.DAILY_FEE * H.DATE ) / 100 * (100 -
+(CASE
+    WHEN H.DATE >= 90 THEN (SELECT DISCOUNT_RATE FROM CTE WHERE DURATION_TYPE = '90일 이상')
+    WHEN H.DATE >= 30 THEN (SELECT DISCOUNT_RATE FROM CTE WHERE DURATION_TYPE = '30일 이상')
+    WHEN H.DATE >= 7  THEN (SELECT DISCOUNT_RATE FROM CTE WHERE DURATION_TYPE = '7일 이상')
+    ELSE 0 
+END)), UNSIGNED) FEE 
+FROM 
+CAR_RENTAL_COMPANY_CAR C JOIN (SELECT *, (DATEDIFF(END_DATE, START_DATE) + 1) DATE
+                               FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY) H
+ON C.CAR_ID = H.CAR_ID 
+WHERE C.CAR_TYPE = '트럭'
 ORDER BY 2 DESC, 1 DESC
+
+
+
 
 # -- 코드를 입력하세요
 # WITH TRUCK AS (
