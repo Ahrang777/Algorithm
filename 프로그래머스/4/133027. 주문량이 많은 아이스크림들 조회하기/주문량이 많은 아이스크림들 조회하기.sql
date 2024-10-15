2f38027936f04e3238b1e0b4645496a7de19c14e
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-10-15 19:10:54 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,16 +1,16 @@
 -- 코드를 입력하세요
-SELECT f.flavor "FLAVOR"
-from first_half f
-join (select flavor, sum(total_order) as july_total_order
-     from july
-     group by flavor) j
-on f.flavor = j.flavor
-order by f.total_order + july_total_order desc
-limit 3;
+WITH JULY_FLAVOR AS (
+    SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOTAL_ORDER
+    FROM JULY
+    GROUP BY FLAVOR
+)
 
+SELECT F.FLAVOR
+FROM FIRST_HALF AS F
+JOIN JULY_FLAVOR AS J
+USING (FLAVOR)
+ORDER BY F.TOTAL_ORDER + J.TOTAL_ORDER DESC
+LIMIT 3;
 
-
--- 7월 총 주문량 + 상반기 총 주문량 큰 순서대로 조회
-
--- first_half >> flavor: pk, shipment_id: fk
--- july >> shipment_id: pk, flavor: fk
+# FIRST_HALF >> SHIPMENT_ID(FK), FLAVOR(PK), TOTAL_ORDER
+# JULY >> SHIPMENT_ID(PK), FLAVOR(FK), TOTAL_ORDER >> 동일한 FLAVOR, 다른 SHIPMENT_ID 가능
