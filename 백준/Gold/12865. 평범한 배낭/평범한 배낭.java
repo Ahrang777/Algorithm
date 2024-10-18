f24216ee67e5dd7eb51abc36323c20f691e3020f
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-10-18 16:40:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 128 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N, K;
@@ -13,8 +10,9 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N + 1][2];    // i번 물건의 무게 W, 가치 V
-        int[] dp = new int[K + 1];  // 무게 i까지 최대 가치
+        // 배낭 / 무게, 가치
+        int[][] arr = new int[N + 1][2];
+        int[] dp = new int[K + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -28,9 +26,6 @@ public class Main {
             int v = arr[i][1];
 
             for (int j = K; j >= w; j--) {
-                // 배낭에 i번째 물건을 담을 수 있는 경우는 이전 최대 가치를 그대로 갖고있으면 된다.
-                // 2차원 배열로 생각했을 때 dp[i][j] = dp[i-1][j]
-                // 배낭에 i번째 물건을 담을 수 있는 경우
                 dp[j] = Math.max(dp[j], dp[j - w] + v);
             }
         }
