16e3fb5c47f1bab4f123e212b6763ef0f0ae7524
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-10-23 14:10:11 +0900

    [Gold III] Title: 앱, Time: 128 ms, Memory: 15360 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 public class Main {
     static int N, M;
-    static int[] memories, costs;
-    static final int INF = (int) 1e9;
+    static int[] memories, costs, dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -12,32 +11,42 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        memories = new int[N + 1];
-        costs = new int[N + 1];
+        memories = new int[N];
+        costs = new int[N];
+
+        // 앱 최대 100개 * 비용 최대 100 = 10000 까지는 입력이 가능해야 한다.
+        // 해당 비용으로 얻을 수 있는 최대 메모리
+        dp = new int[10001];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             memories[i] = Integer.parseInt(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             costs[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[M + 1];
-        Arrays.fill(dp, INF);
+        Arrays.fill(dp, -1);
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = M; j > 0; j--) {
-                if (j <= memories[i]) {
-                    dp[j] = Math.min(dp[j], costs[i]);
-                } else {
-                    dp[j] = Math.min(dp[j], dp[j - memories[i]] + costs[i]);
+        for (int i = 0; i < N; i++) {
+            int cost = costs[i];
+
+            for (int j = 10000; j >= cost; j--) {
+                if (dp[j - cost] != -1) {
+                    dp[j] = Math.max(dp[j], dp[j - cost] + memories[i]);
                 }
             }
+
+            dp[cost] = Math.max(dp[cost], memories[i]);
         }
 
-        System.out.println(dp[M]);
+        for (int i = 0; i < 10001; i++) {
+            if (dp[i] >= M) {
+                System.out.println(i);
+                break;
+            }
+        }
     }
 }
