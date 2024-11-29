8a435ea96ea6fbca5f27e1d27eb09ce2e48ab093
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2024-11-29 17:15:21 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 2.88 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.java
@@ -1,57 +1,98 @@
-// class Solution {
-//     static int N, M, rsx, rsy, rex, rey, bsx, bsy, bex, bey;
-//     static int[] dx = {-1, 0, 1, 0};
-//     static int[] dy = {0, 1, 0, -1};
-//     public int solution(int[][] maze) {
-//         int answer = 0;
-        
-//         N = maze.length;
-//         M = maze[0].length;
-        
-//         for (int i = 0; i < N; i++) {
-//             for (int j = 0; j < M; j++) {
-//                 if (maze[i][j] == 1) {
-//                     rsx = i;
-//                     rsy = j;
-//                 } else if (maze[i][j] == 2) {
-//                     bsx = i;
-//                     bsy = j;
-//                 } else if (maze[i][j] == 3) {
-//                     rex = i;
-//                     rey = j;
-//                 } else if (maze[i][j] == 4) {
-//                     bex = i;
-//                     bey = j;
-//                 }
-//             }
-//         }
-        
-//         return answer;
-//     }
-// }
-
 import java.util.*;
 class Solution {
-    static int answer = Integer.MAX_VALUE;
-    static int [] arx = {-1,1,0,0};
-    static int [] ary = {0,0,-1,1};
-    static boolean [][] red;
-    static boolean [][] blue;
-    static int [][] map; 
-    static int a, b;
-    static int rex, rey, bex, bey;
-    // 0: 빈칸 1: 빨간 시작 2: 파란 시작 3: 빨간 도착 4: 파란 도착 5: 벽 
+    static int N, M, rx, ry, rex, rey, bx, by, bex, bey, answer = Integer.MAX_VALUE;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[][] red, blue;
+    static int[][] map;
     public int solution(int[][] maze) {
-        int rx = 0;
-        int ry = 0;
-        int bx = 0;
-        int by = 0;
-        a = maze.length; b = maze[0].length;
-        map = new int [a][b];      // maze copy 배열 
-        red  = new boolean [a][b]; // 빨간 방문 배열 
-        blue = new boolean [a][b]; // 파란 방문 배열 
-        for(int i = 0; i < a; i++){
-            for(int j = 0; j < b; j++){
+        init(maze);
+        
+        move(rx, ry, bx, by, 0, false, false);
+        
+        return answer == Integer.MAX_VALUE ? 0 : answer;
+    }
+    
+    private void move(int rx, int ry, int bx, int by, int cnt, boolean rend, boolean bend) {
+        if (cnt >= answer)  return;
+        if (!rend && rx == rex && ry == rey) rend = true;
+        if (!bend && bx == bex && by == bey) bend = true;
+        
+        if (rend && bend) {
+            answer = cnt;
+            return;
+        }
+        
+        // 여기서 만약 새로 리스트 생성하는게 아니라 clear로 초기화 하는거면 
+        // java.util.ConcurrentModificationException 예외 발생
+        // 새로 리스트 생성하면 heap 메모리에 이전 리스트 남아있음
+        // 재귀로 돌면서 현재는 새 리스트를 만들지만 해당 재귀 함수 호출한 이전 함수에서는 이전 리스트를 여전히 사용중 아래 이중 for문 부분
+        // 따라서 이전 리스트를 아예 clear로 비우면 이중 for문으로 이용하다가 문제 발생
+        // 따라서 그냥 새 리스트를 생성해서 이전 기록은 남겨놔야 한다. 
+        List<int[]> nr = new ArrayList<>();
+        List<int[]> nb = new ArrayList<>();
+        
+        // 빨간 수레가 이동 가능한 칸 목록
+        if (!rend) {
+            for (int i = 0; i < 4; i++) {
+                int nx = rx + dx[i];
+                int ny = ry + dy[i];
+                
+                if (!isRange(nx, ny) || red[nx][ny] || map[nx][ny] == 5)   continue;
+                
+                nr.add(new int[]{nx, ny});
+            }
+        } else {    // 빨간 수레가 이미 도착한 경우는 해당 위치만 담으면 된다. 파란 수레와 비교하기 위해 필요
+            nr.add(new int[]{rx, ry});
+        }
+        
+        // 파란 수레가 이동 가능한 칸 목록
+        if (!bend) {
+            for (int i = 0; i < 4; i++) {
+                int nx = bx + dx[i];
+                int ny = by + dy[i];
+                
+                if (!isRange(nx, ny) || blue[nx][ny] || map[nx][ny] == 5)   continue;
+                
+                nb.add(new int[]{nx, ny});
+            }
+        } else {
+            nb.add(new int[]{bx, by});
+        }
+        
+        // 빨간, 파란 수레가 이동 가능한 칸 목록 비교하며 가능한 경우로 dfs
+        for (int[] r : nr) {
+            for (int[] b : nb) {
+                // 같은 위치
+                if (r[0] == b[0] && r[1] == b[1])   continue;
+                
+                // 서로 위치 바꾸기
+                if (r[0] == bx && r[1] == by && b[0] == rx && b[1] == ry) {
+                    continue;
+                }
+                
+                red[r[0]][r[1]] = true;
+                blue[b[0]][b[1]] = true;
+                
+                move(r[0], r[1], b[0], b[1], cnt + 1, rend, bend);
+                    
+                red[r[0]][r[1]] = false;
+                blue[b[0]][b[1]] = false;
+            }
+        }
+    }
+    
+    
+    private void init(int[][] maze) {
+        N = maze.length;
+        M = maze[0].length;
+        
+        red = new boolean[N][M];       
+        blue = new boolean[N][M];
+        map = new int[N][M];
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
                 if(maze[i][j] == 0) continue;
                 
                 if(maze[i][j] == 1){     // 빨간 시작 
@@ -75,61 +116,9 @@ class Solution {
                 }
             }
         }
-        // 백트래킹 시작 
-        move(rx, ry, bx, by, 0, false, false);  
-      
-        return answer == Integer.MAX_VALUE ? 0 : answer;
     }
-    public static void move(int rx, int ry, int bx, int by, int move, boolean red_end, boolean blue_end){
     
-        if(!red_end && rx == rex && ry == rey) red_end = true;
-        if(!blue_end && bx == bex && by == bey) blue_end = true;
-        
-        if(red_end && blue_end){
-            answer = Math.min(answer, move);
-            return;
-        }
-        
-        ArrayList<int[]> r_list = new ArrayList<>(); // 빨강이 이동 가능한 
-        ArrayList<int[]> b_list = new ArrayList<>(); // 파랑이 이동 가능한 
-        
-        if(!red_end){
-            for(int i = 0; i < 4; i++){
-                int nx = arx[i] + rx;
-                int ny = ary[i] + ry;
-                if(validation(nx, ny) && map[nx][ny] != 5 && !red[nx][ny]) r_list.add(new int []{nx, ny});
-            }
-        }
-        else r_list.add(new int []{rx, ry});
-                
-        if(!blue_end){
-            for(int i = 0; i < 4; i++){
-                int nx = arx[i] + bx;
-                int ny = ary[i] + by;
-                if(validation(nx, ny) && map[nx][ny] != 5 && !blue[nx][ny]) b_list.add(new int []{nx, ny});
-            }
-        }
-        else b_list.add(new int []{bx, by});
-        
-        for(int i = 0; i < r_list.size(); i++){
-            int [] r_arr = r_list.get(i);
-            for(int j = 0; j < b_list.size(); j++){
-                int [] b_arr = b_list.get(j);
-                // 서로 같은 위치로 이동 X
-                if(r_arr[0] == b_arr[0] && r_arr[1] == b_arr[1]) continue;
-                // 서로 자리 바꾸기 X
-                if(rx == b_arr[0] && ry == b_arr[1] && bx == r_arr[0] && by == r_arr[1]) continue; 
-                
-                red[r_arr[0]][r_arr[1]] = true;
-                blue[b_arr[0]][b_arr[1]] = true;
-                move(r_arr[0], r_arr[1], b_arr[0], b_arr[1], move + 1, red_end, blue_end);
-                red[r_arr[0]][r_arr[1]] = false;
-                blue[b_arr[0]][b_arr[1]] = false;
-            }
-        }
-    }
-    public static boolean validation(int nx, int ny){ // ArrayOutOfBoundsIndex 예외 방지 
-        if(0 <= nx && 0 <= ny && nx < a && ny < b) return true;
-        return false;
+    private boolean isRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
     }
 }
