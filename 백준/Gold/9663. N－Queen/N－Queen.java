4188a3f6dcf75ed6410bf89dec60b892330ab8e9
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2025-02-02 15:51:16 +0900

    [Gold IV] Title: N-Queen, Time: 5240 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,43 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N, result;
-    static int[] output;    // 해당 행(idx)의 열(요소)
+    static int[] queens;
+    static boolean[] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        output = new int[N];
 
-        solve(0);
+        N = Integer.parseInt(br.readLine());
+
+        queens = new int[N];
+        visited = new boolean[N];
+
+        dfs(0);
+
         System.out.println(result);
     }
 
-    private static void solve(int row) {
-        if (row == N) {
+    private static void dfs(int cnt) {
+        if (cnt == N) {
             result++;
             return;
         }
 
-        // col >> i
         for (int i = 0; i < N; i++) {
-            output[row] = i;
-            if (!isValid(row)) {
-                continue;
+            queens[cnt] = i;
+            if (isValid(cnt)) {
+                dfs(cnt + 1);
             }
-            solve(row + 1);
         }
     }
 
-    // 행의 경우 row를 1씩 증가하면서 처리했기에 output 의 idx를 1씩 증가하면서 처리했기에 ok
-    private static boolean isValid(int row) {
-        for (int i = 0; i < row; i++) {
-            // col 체크
-            if (output[i] == output[row])	return false;
-
-            // 대각선 체크 >> row의 차이 절대값, col 차이 절대값 동일하면 대각선 >> ex) row: -2, col: +2
-            if (Math.abs(i - row) == Math.abs(output[i] - output[row]))	return false;
+    // cnt번째에 놓은 퀸과 이전 퀸들 비교
+    private static boolean isValid(int cnt) {
+        for (int i = 0; i < cnt; i++) {
+            if (queens[i] == queens[cnt] || cnt - i == Math.abs(queens[i] - queens[cnt])) {
+                return false;
+            }
         }
 
         return true;
