df3eec1babcdd2ff51dffe173bcdf44b982572af
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2025-02-06 23:12:38 +0900

    [Gold V] Title: 제곱수 찾기, Time: 112 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.java
+++ 백준/Gold/1025. 제곱수 찾기/제곱수 찾기.java
@@ -1,73 +1,68 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, M, max;
-    static int[][] map;
+    static int N, M, result;
+    static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        result = -1;
 
-        map = new int[N][M];
-
+        arr = new int[N][M];
         for (int i = 0; i < N; i++) {
             String str = br.readLine();
+
             for (int j = 0; j < M; j++) {
-                map[i][j] = str.charAt(j) - '0';
+                arr[i][j] = str.charAt(j) - '0';
             }
         }
 
-        max = -1;
-
-        // 시작점
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                checkSquareNumber(i, j);
+                checkSqure(i, j);
             }
         }
 
-        System.out.println(max);
+        System.out.println(result);
     }
 
-    private static void checkSquareNumber(int x, int y) {
-        for (int i = -N; i < N; i++) {  // x의 증가 크기
-            for (int j = -M; j < M; j++) {  // y의 증가 크기
-
-                // 크기 변화가 없으므로 제외
+    private static void checkSqure(int x, int y) {
+        // x, y에서 왼쪽, 오른쪽으로 가능한 끝까지 변경해가며 완전제곱수인지 확인
+        for (int i = -N; i < N; i++) {
+            for (int j = -M; j < M; j++) {
                 if (i == 0 && j == 0) {
                     continue;
                 }
 
-                // 0 ~ N , 0 ~ M 범위 내에 있는지
-                // 해당 x, y의 크기변화로 끝까지 적용
                 int nx = x;
                 int ny = y;
-                int result = 0;
+                int total = 0;
+
                 while (isRange(nx, ny)) {
-                    result = result * 10 + map[nx][ny];
+                    total = total * 10 + arr[nx][ny];
                     nx += i;
                     ny += j;
 
-                    if (isSquare(result)) {
-                        max = Math.max(max, result);
+                    if (isSqure(total)) {
+                        result = Math.max(result, total);
                     }
                 }
             }
         }
+
     }
 
     private static boolean isRange(int x, int y) {
         return x >= 0 && x < N && y >= 0 && y < M;
     }
 
-    private static boolean isSquare(int num) {
+    private static boolean isSqure(int num) {
         int sqrt = (int) Math.sqrt(num);
 
-        return num == sqrt * sqrt;
+        return sqrt * sqrt == num;
     }
 }
