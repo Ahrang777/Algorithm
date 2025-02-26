56b646684ed1c5c571e7c485d42a09a66e055441
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2025-02-26 23:17:02 +0900

    [Gold IV] Title: 스도쿠, Time: 584 ms, Memory: 19608 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.java
+++ 백준/Gold/2239. 스도쿠/스도쿠.java
@@ -3,75 +3,76 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	static int[][] map;
-	static final int SIZE = 9;
-	static boolean end;
+    final static int N = 9;
+    static boolean isEnd;
+    static int[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
+        map = new int[N][N];
 
-		map = new int[SIZE][SIZE];
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < N; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
 
-		for (int i = 0; i < SIZE; i++) {
-			String str = br.readLine();
-			for (int j = 0; j < SIZE; j++) {
-				map[i][j] = str.charAt(j) - '0';
-			}
-		}
+        dfs(0);
 
-		dfs(0);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(map[i][j]);
+            }
+            sb.append("\n");
+        }
 
-		for (int i = 0; i < SIZE; i++) {
-			for (int j = 0; j < SIZE; j++) {
-				sb.append(map[i][j]);
-			}
-			sb.append("\n");
-		}
+        System.out.println(sb);
+    }
 
-		System.out.println(sb);
-	}
+    private static void dfs(int cnt) {
+        if (cnt == 81) {
+            isEnd = true;
+            return;
+        }
 
-	private static void dfs(int depth) {
-		if (depth == 81) {
-			end = true;
+        int x = cnt / N, y = cnt % N;
 
-			return;
-		}
+        if (map[x][y] == 0) {
+            for (int i = 1; i <= N; i++) {
+                if (!isValid(x, y, i)) {
+                    continue;
+                }
 
-		int x = depth / SIZE;
-		int y = depth % SIZE;
+                map[x][y] = i;
+                dfs(cnt + 1);
+                if (isEnd) {
+                    return;
+                }
+                map[x][y] = 0;
+            }
+        } else {
+            dfs(cnt + 1);
+        }
+    }
 
-		if (map[x][y] != 0) {
-			dfs(depth + 1);
-		} else {
-			for (int i = 1; i <= 9; i++) {
-				if (!isValid(x, y, i))
-					continue;
+    private static boolean isValid(int x, int y, int num) {
+        for (int i = 0; i < N; i++) {
+            if (map[x][i] == num || map[i][y] == num) {
+                return false;
+            }
+        }
 
-				map[x][y] = i;
-				dfs(depth + 1);
-				if (end)	return;	// 한번 완성되면 그 이후는 처음 값보다 큰값이므로
-				map[x][y] = 0;
-			}
-		}
-	}
+        x = x / 3 * 3;
+        y = y / 3 * 3;
 
-	private static boolean isValid(int x, int y, int num) {
-		// 가로 , 세로
-		for (int i = 0; i < SIZE; i++) {
-			if (map[x][i] == num || map[i][y] == num)	return false;
-		}
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                if (map[i][j] == num)   return false;
+            }
+        }
 
-		// 3 x 3 체크
-		x = x / 3 * 3;
-		y = y / 3 * 3;
-		for (int i = x; i < x + 3; i++) {
-			for (int j = y; j < y + 3; j++) {
-				if (map[i][j] == num)	return false;
-			}
-		}
-
-		return true;
-	}
+        return true;
+    }
 }
