ba7e98ebfb99dc6ebe16cb624bcadcb689e5f32a
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2025-03-15 17:31:13 +0900

    [Gold V] Title: 토마토, Time: 540 ms, Memory: 108640 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,102 +1,86 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/**
- * https://www.acmicpc.net/problem/7576
- * @author SSAFY
- *
- */
 public class Main {
-	static class Position {
-		int x, y, cnt;
+    static class Position {
+        int x, y, time;
 
-		public Position(int x, int y, int cnt) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-		}
-	}
-	
-	static int N, M, map[][], min;
-	static List<Position> list;	
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, -1, 0, 1};
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());		
-		
-		N = Integer.parseInt(st.nextToken());	// 가로
-		M = Integer.parseInt(st.nextToken());	// 세로
-		
-		map = new int[M][N];
-		min = Integer.MAX_VALUE;
-		list = new ArrayList<>();
-		
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				
-				if (map[i][j] == 1) {
-					list.add(new Position(i, j, 0));
-				}
-			}
-		}
-		
-		bfs();
-		
-		if (!isRipeTomatoes()) {
-			min = -1;
-		}
-		
-		System.out.println(min);
-	}
-	
-	// 모든 토마토가 익었는지 여부 
-	private static boolean isRipeTomatoes() {
-		for (int i = 0; i < M; i++) {
-			for (int j = 0; j < N; j++) {
-				if (map[i][j] == 0) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	
-	private static void bfs() {
-		Queue<Position> q = new ArrayDeque<>();
-		
-		for (Position p : list) {
-			q.offer(p);
-		}
-		
-		while (!q.isEmpty()) {
-			Position now = q.poll();
-			min = now.cnt;
-			
-			for (int i = 0; i < 4; i++) {
-				int nx = now.x + dx[i];
-				int ny = now.y + dy[i];
-				
-				if (!isRange(nx, ny))	continue;
-				
-				
-				if (map[nx][ny] == 0) {
-					q.offer(new Position(nx, ny, now.cnt + 1));
-					map[nx][ny] = 1;	
-				}				
-			}
-		}
-	}
-	
-	private static boolean isRange(int x, int y) {
-		return x >= 0 && x < M && y >= 0 && y < N;
-	}
+        public Position(int x, int y, int time) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+        }
+    }
+    static int N, M, result;
+    static int[][] map;
+    static List<Position> list;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        list = new ArrayList<>();
+
+        // 토마토 개수
+        int total = N * M;
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if (map[i][j] == 1) {
+                    list.add(new Position(i, j, 0));
+                }
+
+                if (map[i][j] == -1) {
+                    total--;
+                }
+            }
+        }
+
+        bfs(total);
+
+        System.out.println(result);
+    }
+
+    private static void bfs(int total) {
+        Queue<Position> q = new ArrayDeque<>();
+
+        for (Position p : list) {
+            q.offer(p);
+        }
+
+        while (!q.isEmpty()) {
+            Position now = q.poll();
+            total--;
+
+            result = Math.max(result, now.time);
+
+            for (int i = 0; i < 4; i++) {
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
+
+                if (!isRange(nx, ny) || map[nx][ny] != 0) {
+                    continue;
+                }
+
+                q.offer(new Position(nx, ny, now.time + 1));
+                map[nx][ny] = 1;
+            }
+        }
+
+        if (total != 0) {
+            result = -1;
+        }
+    }
+
+    private static boolean isRange(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
+    }
 }
