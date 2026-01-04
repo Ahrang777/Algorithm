073ba4a840603896e8be0599a6743468a1052559
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2026-01-05 00:41:16 +0900

    [Silver II] Title: DFS와 BFS, Time: 216 ms, Memory: 20432 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,88 +1,83 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-/**
- * https://www.acmicpc.net/problem/1260
- * 
- * @author SSAFY
- *
- */
 public class Main {
-	static int N, M, V;
-	static List<Integer>[] graph;
-	static boolean[] visited;
+    static int N, M, V;
+    static List<Integer>[] graph;
+    static StringBuilder sb;
+    static boolean[] visited;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		V = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
-		visited = new boolean[N + 1];
-		graph = new List[N + 1];
-		
-		for (int i = 1; i <= N; i++) {
-			graph[i] = new ArrayList<>();
-		}
+        visited = new boolean[N + 1];
+        graph = new List[N + 1];
 
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-			graph[a].add(b);
-			graph[b].add(a);
-		}
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i <= N; i++) {
-			Collections.sort(graph[i]);
-		}
-		
-		dfs(V, sb);
-		sb.append("\n");
-		
-		Arrays.fill(visited, false);
-		bfs(V, sb);
-		
-		System.out.println(sb);
-	}
-	
-	private static void dfs(int start, StringBuilder sb) {
-		visited[start] = true;
-		sb.append(start).append(" ");
-		
-		for (int next : graph[start]) {
-			if (!visited[next]) {
-				dfs(next, sb);
-			}
-		}
-	}
-	
-	private static void bfs(int start, StringBuilder sb) {
-		Queue<Integer> q = new ArrayDeque<>();		
-		q.offer(start);
-		visited[start] = true;
-		
-		while (!q.isEmpty()) {
-			int now = q.poll();
-			sb.append(now).append(" ");
-			
-			for (int next : graph[now]) {
-				if (!visited[next]) {
-					q.offer(next);
-					visited[next] = true;
-				}
-			}
-		}
-	}
+            graph[from].add(to);
+            graph[to].add(from);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+        }
+
+        dfs(V);
+
+        sb.append("\n");
+        Arrays.fill(visited, false);
+
+        bfs(V);
+
+        System.out.println(sb);
+    }
+
+    private static void dfs(int now) {
+        visited[now] = true;
+        sb.append(now).append(" ");
+
+        for (int next : graph[now]) {
+            if (visited[next]) {
+                continue;
+            }
+
+            dfs(next);
+        }
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> q = new ArrayDeque<>();
+
+        q.offer(start);
+        visited[start] = true;
+        sb.append(start).append(" ");
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            for (int next : graph[now]) {
+                if (visited[next]) {
+                    continue;
+                }
+
+                q.offer(next);
+                visited[next] = true;
+                sb.append(next).append(" ");
+            }
+        }
+    }
 }
