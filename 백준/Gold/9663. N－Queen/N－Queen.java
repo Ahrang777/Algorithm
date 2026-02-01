b144dcc2c9e4d4e733d1dcc15c3d7b528d9d2691
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2026-02-01 16:01:44 +0900

    [Gold IV] Title: N-Queen, Time: 5088 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -2,39 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, result;
-    static int[] queens;
-    
+    static int N, cnt;
+    static int[] queen;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
 
-        queens = new int[N];       
+        queen = new int[N];
 
         dfs(0);
 
-        System.out.println(result);
+        System.out.println(cnt);
     }
 
-    private static void dfs(int cnt) {
-        if (cnt == N) {
-            result++;
+    private static void dfs(int x) {
+        if (x == N) {
+            cnt++;
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            queens[cnt] = i;
-            if (isValid(cnt)) {
-                dfs(cnt + 1);
+        for (int y = 0; y < N; y++) {
+            queen[x] = y;
+
+            if (isPossible(x)) {
+                dfs(x + 1);
             }
         }
     }
 
-    // cnt번째에 놓은 퀸과 이전 퀸들 비교
-    private static boolean isValid(int cnt) {
-        for (int i = 0; i < cnt; i++) {
-            if (queens[i] == queens[cnt] || cnt - i == Math.abs(queens[i] - queens[cnt])) {
+    private static boolean isPossible(int x) {
+        for (int i = 0; i < x; i++) {
+            if (queen[i] == queen[x] || x - i == Math.abs(queen[x] - queen[i])) {
                 return false;
             }
         }
