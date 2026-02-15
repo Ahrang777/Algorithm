a8aeb8f62dc41f112c1289fe660c650c8b4880b3
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2026-02-15 15:45:11 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 96 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.java
@@ -8,9 +8,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+
         A = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
+        /*
         dp = new int[K + 1];
 
         for (int i = A + 1; i <= K; i++) {
@@ -22,5 +24,22 @@ public class Main {
         }
 
         System.out.println(dp[K]);
+
+        */
+
+        int cnt = 0;
+
+        // K -> A로 만들면서 연산횟수를 계산한다.
+        while (K != A) {
+            if (K % 2 == 0 && K / 2 >= A) {
+                K /= 2;
+            } else {
+                K--;
+            }
+
+            cnt++;
+        }
+
+        System.out.println(cnt);
     }
 }
