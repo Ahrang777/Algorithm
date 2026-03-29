484cf041dfce3a867bf6f1a70f27554461e246b6
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2026-03-29 19:17:50 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,23 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, K;
+    static int T, N, M, K;
     static int[][] map;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = null;
+        StringBuilder sb = new StringBuilder();
+        List<int[]> positions = new ArrayList<>();
 
-        int T = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
 
         for (int tc = 1; tc <= T; tc++) {
+            positions.clear();
+            int cnt = 0;
             st = new StringTokenizer(br.readLine());
 
-            int answer = 0;
-            M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
 
             map = new int[N][M];
@@ -26,22 +28,25 @@ public class Main {
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
 
-                int y = Integer.parseInt(st.nextToken());
                 int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
 
+                positions.add(new int[]{x, y});
                 map[x][y] = 1;
             }
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (map[i][j] == 1) {
-                        answer++;
-                        bfs(i, j);
-                    }
+            for (int[] p : positions) {
+                int x = p[0], y = p[1];
+
+                if (map[x][y] == 0) {
+                    continue;
                 }
+
+                bfs(x, y);
+                cnt++;
             }
 
-            sb.append(answer).append("\n");
+            sb.append(cnt).append("\n");
         }
 
         System.out.println(sb);
@@ -65,8 +70,8 @@ public class Main {
                     continue;
                 }
 
-                q.offer(new int[]{nx, ny});
                 map[nx][ny] = 0;
+                q.offer(new int[]{nx, ny});
             }
         }
     }
