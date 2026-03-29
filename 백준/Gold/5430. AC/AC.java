44289a7500a524459bfaf28481c5b14d46c260a6
JungIn Park <59478159+Ahrang777@users.noreply.github.com>
2026-03-29 18:37:42 +0900

    [Gold V] Title: AC, Time: 772 ms, Memory: 92012 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,93 +1,79 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int tc = 1; tc <= T; tc++) {
-			String commands = br.readLine();
-			N = Integer.parseInt(br.readLine());
-			String[] arr = new String[N];
-			
-			String numbers = br.readLine();
-			boolean isReverse = false;	// 뒤집힘 여부
-			boolean isError = false;	// 에러 발생 여부
-			
-			// 투포인터
-			int start = 0, end = N - 1;
-			
-			if (N == 0) {
-				// D 포함한 경우 에러 
-				if (commands.contains("D")) {
-					isError = true;
-				}				
-			} else {				
-				int idx = 0;
-				
-				numbers = numbers.substring(1, numbers.length() - 1);
-				arr = numbers.split(",");
-//				System.out.println(Arrays.toString(arr));
-				
-				while (start <= end) {
-					if (idx >= commands.length())	break;
-					char command = commands.charAt(idx++);
-					
-					if (command == 'R') {	// 뒤집기
-						isReverse = !isReverse;
-					} else {	// 제거
-						if (isReverse) {	// 뒤에서 제거 
-							end--;
-						} else {	// 앞에서 제거 
-							start++;
-						}
-					}
-				}
-				
-				// 아직 남은 command가 있는 경우
-				if (idx < commands.length()) {	
-					commands = commands.substring(idx);
-					
-					// 배열은 비었는데 D 연산이 있는 경우 
-					if (commands.contains("D")) {
-						isError = true;
-					}
-				}
-			}
-			
-						
-			// error 인 경우
-			if (isError) {
-				sb.append("error").append("\n");
-				continue;
-			}
-			
-			sb.append("[");
+    static int T, N;
+    static String P;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> dq = new ArrayDeque<>();
+        boolean isReverse = false;
+        boolean isError = false;
 
-			// 배열 크기 0이 아닌 경우 
-			if (N != 0 && start <= end) {
-				// 뒤집힌 경우 
-				if (isReverse) {
-					for (int i = end; i >= start; i--) {					
-						sb.append(arr[i]).append(",");					
-					}
-				} else { // 기존 순서와 동일
-					for (int i = start; i <= end; i++) {
-						sb.append(arr[i]).append(",");
-					}
-				}
-				sb.deleteCharAt(sb.length() - 1);	// 마지막 , 제거				
-			}
-			
-			sb.append("]").append("\n");
-			
-		}
-		
-		System.out.println(sb);		
-	}
+        T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            sb.setLength(0);
+            isReverse = false;
+            isError = false;
+            dq.clear();
+
+            P = br.readLine();
+            N = Integer.parseInt(br.readLine());
+
+            if (N > 0) {
+                String input = br.readLine();
+                input = input.substring(1, input.length() - 1);
+
+                String[] arr = input.split(",");
+
+                for (int i = 0; i < N; i++) {
+                    dq.offer(Integer.parseInt(arr[i]));
+                }
+            } else {
+                br.readLine();
+            }
+
+
+            for (int i = 0; i < P.length(); i++) {
+                char c = P.charAt(i);
+
+                if (c == 'R') {
+                    isReverse = !isReverse;
+                } else {
+                    if (dq.isEmpty()) {
+                        sb.append("error");
+                        isError = true;
+                        break;
+                    } else {
+                        if (isReverse) {
+                            dq.pollLast();
+                        } else {
+                            dq.pollFirst();
+                        }
+                    }
+                }
+            }
+
+            if (!isError) {
+                sb.append("[");
+                while (!dq.isEmpty()) {
+                    if (isReverse) {
+                        sb.append(dq.pollLast());
+                    } else {
+                        sb.append(dq.pollFirst());
+                    }
+
+                    if (!dq.isEmpty()) {
+                        sb.append(",");
+                    }
+                }
+
+                sb.append("]");
+            }
+
+            System.out.println(sb);
+        }
+    }
 }
